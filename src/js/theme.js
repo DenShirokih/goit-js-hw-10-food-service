2c8564bc2d29bf70a470caffe454a1667c24adb1
Denis <abcdenis95@gmail.com>
2022-05-13 16:09:22 +0300

    fix name function,fix setDefaultTheme()

--- src/js/theme.js
+++ src/js/theme.js
@@ -4,34 +4,34 @@ const Theme = {
 };
 const inputRef = document.querySelector('#theme-switch-toggle');
 const changeInput = event => {
-  event.currentTarget.checked ? darkTheme() : lightTheme();
+  event.currentTarget.checked ? setDarkTheme() : setLightTheme();
 };
 inputRef.addEventListener('change', changeInput);
 
-const darkTheme = () => {
+const setDarkTheme = () => {
   document.body.classList.add(Theme.DARK);
   document.body.classList.remove(Theme.LIGHT);
   localStorage.setItem('theme', Theme.DARK);
   inputRef.checked = true;
 };
 
-const lightTheme = () => {
+const setLightTheme = () => {
   document.body.classList.add(Theme.LIGHT);
   document.body.classList.remove(Theme.DARK);
   localStorage.setItem('theme', Theme.LIGHT);
   inputRef.checked = false;
 };
 
-const defaultTheme = () => {
+const setDefaultTheme = () => {
   const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === Theme.LIGHT || savedTheme === Theme.LIGHT) {
-    lightTheme();
+  if (savedTheme === Theme.LIGHT || savedTheme === null) {
+    setLightTheme();
     return;
   }
   if (savedTheme === Theme.DARK) {
-    darkTheme();
+    setDarkTheme();
     return;
   }
 };
 
-defaultTheme();
+setDefaultTheme();
